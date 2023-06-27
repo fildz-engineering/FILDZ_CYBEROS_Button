44f2b29c4f9614ae273d4855758c6770fec19384
Edgaras Janušauskas <edgaras@fildz.com>
2023-06-27 13:10:43 +0300

    Fix mistakes in the comments.

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@
 # Features:
 # - Completely asynchronous.
 # - Supports down, hold, up, click and, double-click events.
-# - Adjustable debounce and double click ms.
+# - Adjustable debounce and double-click ms.
 # - Supports user-defined coroutines.
 
 import fildz_cyberos as cyberos
